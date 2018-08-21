dbfe4b97860ce8ecd550c07762337e1303c3db19
Piotrek <piotrek120017@gmail.com>
2018-08-21 23:16:51 +0200

    fixes in prepare image for blank images

--- getLetter.m
+++ getLetter.m
@@ -1,12 +1,17 @@
 function[letter] = getLetter(img)
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ,'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ,'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-if exist('net.mat', 'file') == 2
-    load('net.mat');
-else
-    net = net_build();
+    if exist('net.mat', 'file') == 2
+        load('net.mat');
+    else
+        net = net_build();
+    end
+
+    [img, err] = prepareImage(img);
+    if err ~= 0 
+        letter = err;
+    else
+        letter = letters(classify(net, img));
+    end
 end
-
-img = prepareImage(img);
-letter = letters(classify(net, img));
