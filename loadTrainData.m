f15917777ad6f01a2025b7f8224bbfd9cc38b460
Piotrek <piotrek120017@gmail.com>
2018-08-25 17:04:20 +0200

    fix indexing in loading train data script

--- loadTrainData.m
+++ loadTrainData.m
@@ -7,11 +7,11 @@ function [images, images_test, labels, labels_test] =  loadTrainData()
     images = uint8(zeros(28,28, 1, 124800));
     labels_test = categorical(uint8(dataset.test.labels(:))');
     images_test = uint8(zeros(28,28, 1, 20800));
-    for i = 1: 124800
+    for i = 1: size(temp, 1)
         images(:, :, i) = reshape(temp(i,:),[28,28,1]);
 
     end
-    for j = 1: 20800
+    for j = 1: size(temp_test, 1)
         images_test(:, :, j) = reshape(temp_test(j,:),[28,28,1]);
     end
 end
