6991bf9595e4d571bc7977ce454404c1fafa38d2
Piotrek <piotrek120017@gmail.com>
2018-12-06 21:23:18 +0100

    change answered letter to uppercase

--- getLetter.m
+++ getLetter.m
@@ -3,7 +3,7 @@
 %1 - blank image
 function[letter, err] = getLetter(img)
 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ,'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M' ,'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
     if exist('net.mat', 'file') == 2
         load('net.mat');
